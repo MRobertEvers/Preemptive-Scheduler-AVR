4897a08613f5b47393b91af47faa7ecfdd07c0dc
MRobertEvers <mreverssci@gmail.com>
2018-01-12 18:04:32 -0600

    Some comment changes

--- main.c
+++ main.c
@@ -40,17 +40,11 @@ int main(void)
 // http://www.microchip.com/webdoc/avrlibcreferencemanual/group__avr__interrupts.html
 ISR(TIMER0_COMPA_vect)
 {
-	// Do scheduler stuff.
-	// If we call a function with a function pointer as its argument,
-	// since pointers are 2 bytes long, those two bytes will be in
-	// R26 and R25. We can use that knowledge the make the "return" address that function.
 	// The program counter is on the stack at this point. If we were to leave this context,
 	// then upon return, the exit procedure for leaving an ISR is to pop the return address
 	// off the stack. Since the return address for an ISR will be the address that was
 	// interrupted, pointing to the stack pointer of this context will cause that address
 	// to be returned to.
 	// TODO: Explain this better.
-	cli();
 	megos_schedule();
-	sei();
 }
