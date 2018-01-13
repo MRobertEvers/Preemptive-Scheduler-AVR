07ce29e9c7f471007a22c4f9089db77055d0bbd4
MRobertEvers <mreverssci@gmail.com>
2018-01-12 18:02:01 -0600

    Remove ignored files

--- Debug/megos.d
+++
@@ -1,5 +0,0 @@
-megos.d megos.o: .././megos.c .././megos.h .././megos_system.h
-
-.././megos.h:
-
-.././megos_system.h:
--- Debug/megos_context_switch.d
+++
@@ -1,2 +0,0 @@
-megos_context_switch.d megos_context_switch.o: \
- .././megos_context_switch.s
